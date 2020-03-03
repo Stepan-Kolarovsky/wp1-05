09717e68e5ec5b0f5ba38095ede26328b8044232
Stepan-Kolarovsky <stepan.kolarovsky@student.ossp.cz>
2020-03-03 10:23:05 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  $homelessMoney="25 + 17 + 40 + 100 - 50 "
+  $vodkaPrice="120"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     Cena vodky:<?=$vodkaPrice> ;
+     <br>Peníze bezdomovce před návštěvou večerky: <?=$homelessMoney?>
+</body>
+</html>
